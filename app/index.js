523fe5b7c41c7bec451cdf32c93b3329a30f6974
nineSean <ninesean1989@gmail.com>
2017-05-29 10:15:30 +0800

    error异常处理，‘binary’图片处理

--- app/index.js
+++ app/index.js
@@ -10,17 +10,18 @@ class App {
     return (request, response) => {
 
       let { url } = request
-      let getPath = (url)=>{
-        return path.resolve(process.cwd(),'public',`.${url}`)
+      let getPath = (url) => {
+        return path.resolve(process.cwd(), 'public', `.${url}`)
       }
       let staticFunc = (url) => {
+        if (url === '/') {
+          url = '/index.html'
+        }
         let _path = getPath(url)
-        fs.readFile(_path, 'utf-8', (error, data) => {
-          response.end(data)
+        fs.readFile(_path,'binary' , (error, data) => {
+          if(error) data = `NOT FOUND ${error.stack}`
+          response.end(data,'binary')
         })
-      }
-      if (url === '/') {
-        staticFunc('/index.html')
       }
       staticFunc(url)
     }
